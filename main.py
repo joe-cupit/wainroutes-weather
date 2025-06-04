5d499c12047bd942fab6b490686fdd553ac7b5b6
Joe <jwcupit@gmail.com>
2025-06-04 14:08:01 +0100

    Add metadata to json uploads

--- main.py
+++ main.py
@@ -15,7 +15,32 @@ from src.district_weather import scrape_met_site
 from src.point_weather import request_weather_points
 
 
+def upload_to_bucket(key, obj):
+  try:
+    if obj:
+      json_bytes = json.dumps(
+        obj,
+        separators=(',', ':'),
+        ensure_ascii=False
+      ).encode("utf-8")
+
+      r2_client.put_object(
+        Bucket=os.environ["R2_BUCKET_NAME"],
+        Body=BytesIO(json_bytes),
+        Key=key,
+        ContentType="application/json",
+        CacheControl="public, max-age=3600, stale-while-revalidate=600",
+      )
+      log.info(f"Successfully uploaded '{key}' to R2 Bucket")
+    else:
+      log.warning("No weather object to upload")
+  except Exception as e:
+    log.error(f"Failed to upload '{key}' to R2 Bucket")
+    log.error(e)
+
+
 if __name__ == "__main__":
+  log.info("Starting job.")
 
   log.info("Initialising boto3 client")
   r2_client = boto3.client(
@@ -27,30 +52,15 @@ if __name__ == "__main__":
     config=Config(signature_version='s3v4')
   )
 
+  # fetch weather data
   log.info("Starting the Lake District Weather retreival")
   weather = scrape_met_site()
   weather_points = request_weather_points()
   log.info("Finished the Lake District Weather retreival")
 
-  try:
-    log.info("Converting data and uploading to R2 Bucket")
-    if weather_points:
-      json_bytes = json.dumps(weather).encode("utf-8")
-      json_file_obj = BytesIO(json_bytes)
-      r2_client.upload_fileobj(json_file_obj, os.environ["R2_BUCKET_NAME"], "weather.json")
-      log.info("Successfully uploaded weather.json to R2 Bucket")
-    else:
-      log.warning("No weather object to upload")
-  except Exception as e:
-    log.error("Failed to upload weather.json to R2 Bucket")
+  # save json files to bucket
+  log.info("Converting data and uploading to R2 Bucket")
+  upload_to_bucket("weather.json", weather)
+  upload_to_bucket("weather_points.json", weather_points)
 
-  try:
-    if weather_points:
-      json_bytes = json.dumps(weather_points).encode("utf-8")
-      json_file_obj = BytesIO(json_bytes)
-      r2_client.upload_fileobj(json_file_obj, os.environ["R2_BUCKET_NAME"], "weather_points.json")
-      log.info("Successfully uploaded weather_points.json to R2 Bucket")
-    else:
-      log.warning("No weather_points object to upload")
-  except Exception as e:
-    log.error("Failed to upload weather_points.json to R2 Bucket")
+  log.info("Finished job.")
